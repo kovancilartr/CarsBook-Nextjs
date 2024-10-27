d1eaf1cba71c051f2c03dd8d59473318f3eb51ff
Yalçın <kovancilartr@gmail.com>
2024-10-27 23:04:32 +0300

    Proje seyfaları tamamlandı sadece animasyon işlemleri kaldı

--- app/(routes)/services/page.tsx
+++ app/(routes)/services/page.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import SectionBg from "../_components/SectionBg";
+import AboutPerson from "../_components/Sections/AboutPerson";
+import Testiomonials from "../_components/Sections/Testiomonials";
+import HomeService from "../_components/Sections/HomeService";
 
 const Services = () => {
   return (
-    <div>Services</div>
-  )
-}
+    <div>
+      <SectionBg title="Services" source="Home" />
 
-export default Services
+      <HomeService />
+
+      <AboutPerson />
+      <Testiomonials />
+    </div>
+  );
+};
+
+export default Services;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,37 +9,60 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-	container: {
-		center: true,
-		padding: "2rem",
-		screens: {
-			"2xl": "1400px",
-		},
-	},
+  	container: {
+  		center: 'true',
+  		padding: '2rem',
+  		screens: {
+  			'2xl': '1400px'
+  		}
+  	},
   	extend: {
   		colors: {
-  			myprimary: "#ff3600",
-			mysecondary: "#424242",
-			myLight: {
-				100: "#fff8f6",
-				"bg": "#ffffff",
-			},
-			myDark: {
-				100: "#303030",
-				200: "#fff8f6",
-				"bg": "#000000",
-			},
+  			myprimary: '#ff3600',
+  			mysecondary: '#424242',
+  			myLight: {
+  				'100': '#fff8f6',
+  				bg: '#ffffff'
+  			},
+  			myDark: {
+  				'100': '#303030',
+  				'200': '#fff8f6',
+  				bg: '#000000'
+  			}
   		},
-		backgroundImage: {
-			"footer-bg": "url('/footer-bg.png')",
-			"service-bg": "url('/service-bg.png')",
-			"section-bg": "url('/section-bg.jpg')",
-		},
+  		backgroundImage: {
+  			'footer-bg': 'url('/footer-bg.png',
+  			')': ',
+  			'service-bg': 'url('/service-bg.png',
+  			'section-bg': 'url('/section-bg.jpg'
+  		},
   		borderRadius: {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)',
-			'4xl': '3rem',
+  			'4xl': '3rem'
+  		},
+  		keyframes: {
+  			'accordion-down': {
+  				from: {
+  					height: '0'
+  				},
+  				to: {
+  					height: 'var(--radix-accordion-content-height)'
+  				}
+  			},
+  			'accordion-up': {
+  				from: {
+  					height: 'var(--radix-accordion-content-height)'
+  				},
+  				to: {
+  					height: '0'
+  				}
+  			}
+  		},
+  		animation: {
+  			'accordion-down': 'accordion-down 0.2s ease-out',
+  			'accordion-up': 'accordion-up 0.2s ease-out'
   		}
   	}
   },
